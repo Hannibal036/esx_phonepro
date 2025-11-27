7ecf476ebc7c200fdee333e12fc2155fcebdaa3d
Hannibal036 <109810162+Hannibal036@users.noreply.github.com>
2025-11-27 19:24:06 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.PhoneKey = 'F1'
+Config.Debug = false
+
+-- Apps
+Config.FineMailNotification = true
+Config.EnableRepairHistory = true
+Config.ShowAmbulanceETA = true
+Config.MaxMessagesStored = 500
+
+-- Dispatch
+Config.DispatchRadius = 500.0
+Config.DispatchCooldown = 60
+
+-- PIN / Hintergrund / Klingelton
+Config.EnablePIN = true
+Config.DefaultBackground = "default.jpg"
+Config.EnableRingtone = true
+
+-- Kamera
+Config.EnableCamera = true
+
+-- Voice
+Config.EnableCall = true
+Config.MaxCallDuration = 3600
